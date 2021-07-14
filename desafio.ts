e8a681c333cf30b3a2e100725df7c8ba45cbb0a5
lucas simbron <luvaa30@gmail.com>
2021-07-14 20:03:53 -0300

    otro cambio

--- desafio.ts
+++ desafio.ts
@@ -8,7 +8,7 @@ class Edificio{
         const pisoEncontrado = this.pisos.find(p=>p.nombre==nombreDePiso)
         return pisoEncontrado.pushDepartamento(departamento)
     }
-    getDepartamentoByPiso(nombreDePiso:string): Departamento[]{
+    getDepartamentosByPiso(nombreDePiso:string): Departamento[]{
         const pisoEncontrado = this.pisos.find(p=>p.nombre==nombreDePiso)
         return pisoEncontrado.getDepartamentos();
 
@@ -41,3 +41,34 @@ class Departamento{
 
     }
 }
+function testClaseEdificio() {
+    const unPiso = new Piso("planta baja");
+    const otroPiso = new Piso("primer piso");
+    const unEdificio = new Edificio([unPiso, otroPiso]);
+    const deptoUno = new Departamento("depto uno");
+    const deptoDos = new Departamento("depto dos");
+    const deptoTres = new Departamento("depto tres");
+    unEdificio.addDepartamentoToPiso("planta baja", deptoUno);
+    unEdificio.addDepartamentoToPiso("planta baja", deptoDos);
+    unEdificio.addDepartamentoToPiso("planta baja", deptoTres);
+  
+    const deptos = unEdificio.getDepartamentosByPiso("planta baja");
+    const deptosEmpty = unEdificio.getDepartamentosByPiso("primer piso");
+  
+    if (
+      Array.isArray(deptosEmpty) &&
+      deptosEmpty.length == 0 &&
+      deptos.length == 3 &&
+      deptos[2].getName() == "depto tres"
+    ) {
+      console.log("testClaseBandaApartment passed");
+    } else {
+      throw "testClaseBandaApartment not passed";
+    }
+  }
+  
+  function main() {
+    testClaseEdificio();
+    console.log("otro cambio")
+  }
+  main();
